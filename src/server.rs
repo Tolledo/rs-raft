48a21f34bbfc5d5201a6198a1a29e80d62d46ebe
Tolledo <ps.tolledo@gmail.com>
2021-05-19 23:04:07 -0700

    Use Rc and RefCell to store mut ref to followers

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 #[derive(Debug, PartialEq)]
 enum State {
@@ -12,7 +13,7 @@ struct Server<'a> {
     id: i64,
     current_term: i64,
     voted_for: Option<i64>,
-    followers: Vec<&'a Server<'a>>,
+    followers: Vec<Rc<RefCell<&'a mut Server<'a>>>>,
 }
 
 struct VoteRequest {
@@ -36,35 +37,31 @@ impl<'a> Server<'a> {
         }
     }
 
-    fn add_follower(&mut self, follower: &'a Server<'a>) {
-        self.followers.push(follower)
+    fn add_follower(&mut self, follower: &'a mut Server<'a>) {
+        self.followers.push(Rc::new(RefCell::new(follower)))
     }
 
-    fn add_followers(&mut self, followers: &mut Vec<&'a Server<'a>>) {
-        self.followers.append(followers)
+    fn add_followers(&mut self, followers: Vec<&'a mut Server<'a>>) {
+        for f in followers {
+            self.followers.push(Rc::new(RefCell::new(f)))
+        }
     }
 
-    fn request_vote(mut self) -> bool {
+    fn request_vote(self) -> bool {
         let vote_request = VoteRequest {
             term: self.current_term + 1,
             candidate_id: self.id,
         };
-        let counters = self
-            .followers
-            .iter_mut()
-            .map(|f| f.process_vote_request(&vote_request))
-            .fold(HashMap::new(), |mut acc, resp| {
-                *acc.entry(resp.vote_granted).or_insert(0) += 1;
-                acc
-            });
-
-        match counters.get(&true) {
-            Some(count) => *count >= self.followers.len() / 2,
-            None => false,
+        let mut voted = 0;
+        for follower in &self.followers {
+            let vote_response = follower.borrow_mut().process_vote_request(&vote_request);
+            if vote_response.vote_granted {
+                voted += 1;
+            }
         }
+        return voted >= self.followers.len() / 2;
     }
 
-    //TODO fix issues with borrow checker. Add tokio and try using channels (or something else?)
     fn process_vote_request(&mut self, vote_request: &VoteRequest) -> VoteResponse {
         if vote_request.term < self.current_term {
             return VoteResponse {
@@ -72,7 +69,7 @@ impl<'a> Server<'a> {
                 vote_granted: false,
             };
         };
-        let response = return match self.voted_for {
+        return match self.voted_for {
             Some(candidate_id) => {
                 if candidate_id == vote_request.candidate_id {
                     self.current_term = vote_request.term;
@@ -96,7 +93,6 @@ impl<'a> Server<'a> {
                 }
             }
         };
-        response
     }
 }
 
@@ -115,12 +111,14 @@ mod tests {
     #[test]
     fn request_vote() {
         let mut leader = Server::new(0);
-        let follower_1 = Server::new(1);
-        let follower_2 = Server::new(2);
-        let follower_3 = Server::new(3);
-        let follower_4 = Server::new(4);
-        let mut followers = vec![&follower_1, &follower_2, &follower_3, &follower_4];
-        leader.add_followers(&mut followers);
+        let mut follower_1 = Server::new(1);
+        let mut follower_2 = Server::new(2);
+        let mut follower_3 = Server::new(3);
+        let mut follower_4 = Server::new(4);
+
+        let followers = vec![&mut follower_1, &mut follower_2, &mut follower_3, &mut follower_4];
+
+        leader.add_followers(followers);
 
         assert!(leader.request_vote())
     }
