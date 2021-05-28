b5750044174a8cb74afaa8f112028f77043318a5
Tolledo <ps.tolledo@gmail.com>
2021-05-27 23:44:32 -0700

    Update channels

--- src/server.rs
+++ src/server.rs
@@ -1,27 +1,29 @@
-use async_trait::async_trait;
 use futures::stream::{FuturesUnordered, StreamExt};
-use futures::{
-    future::FutureExt, // for `.fuse()`
-    pin_mut,
-    select,
-};
-use mockall::automock;
-use tokio::sync::{mpsc, oneshot};
+use std::sync::Arc;
 use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::sync::{mpsc, oneshot, Mutex};
 
+pub type Error = Box<dyn std::error::Error + Send + Sync>;
+
+pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Clone, Debug, PartialEq)]
-enum State {
+enum ElectionState {
     Follower,
     Candidate,
     Leader,
 }
 
-struct Server {
-    state: State,
-    id: i64,
+#[derive(Debug)]
+struct State {
+    election_state: ElectionState,
     current_term: i64,
     voted_for: Option<i64>,
+}
+
+struct Server {
+    id: i64,
+    state: Arc<Mutex<State>>,
     rx: Receiver<Command>,
     tx: Sender<Command>,
     nodes: Vec<Sender<Command>>,
@@ -44,24 +46,20 @@ enum Command {
         request: VoteRequest,
         resp: oneshot::Sender<VoteResponse>,
     },
-}
-
-#[async_trait]
-#[automock]
-trait Node: Send + Sync {
-    async fn request_vote(&mut self);
-
-    async fn process_vote_request(&mut self, vote_request: &VoteRequest) -> VoteResponse;
+    RequestVote {},
+    ShutDown {},
 }
 
 impl Server {
     fn new(id: i64) -> Self {
         let (tx, rx) = mpsc::channel(32);
         Server {
-            state: State::Follower,
+            state: Arc::new(Mutex::new(State {
+                election_state: ElectionState::Follower,
+                current_term: 0,
+                voted_for: None,
+            })),
             id,
-            current_term: 0,
-            voted_for: None,
             rx,
             tx,
             nodes: vec![],
@@ -77,164 +75,215 @@ impl Server {
             self.add_node(node)
         }
     }
+}
 
-    async fn start(&mut self) {
-        loop {
-            use Command::*;
+async fn start(
+    mut rx: Receiver<Command>,
+    id: i64,
+    nodes: Vec<Sender<Command>>,
+    self_state: Arc<Mutex<State>>,
+) {
+    use Command::*;
 
-            match self.rx.recv().await {
-                Some(cmd) => {
-                    match cmd {
-                        Vote { request, resp } => {
-                            let res = self.process_vote_request(&request).await;
-                            resp.send(res);
-                        }
-                    }
+    println!("Starting Server id {}", id);
+    loop {
+        match rx.recv().await {
+            Some(cmd) => match cmd {
+                RequestVote {} => {
+                    let self_state = self_state.clone();
+                    let nodes = nodes.clone();
+                    tokio::spawn(async move {
+                        request_vote(id, nodes, self_state).await;
+                    });
                 }
-                None => {}
+                Vote { request, resp } => {
+                    let self_state = self_state.clone();
+                    tokio::spawn(async move {
+                        let res = process_vote_request(self_state, &request).await;
+                        match resp.send(res) {
+                            Ok(_) => {}
+                            Err(_) => {}
+                        }
+                    });
+                }
+                ShutDown { .. } => {
+                    return;
+                }
+            },
+            None => {
+                return;
             }
         }
     }
 }
 
-#[async_trait]
-impl Node for Server {
-    async fn request_vote(&mut self) {
-        self.state = State::Candidate;
-        self.current_term += 1;
-        self.voted_for = Option::from(self.id);
+async fn request_vote(id: i64, nodes: Vec<Sender<Command>>, self_state: Arc<Mutex<State>>) {
+    let mut self_state = self_state.lock().await;
+    self_state.election_state = ElectionState::Candidate;
+    self_state.current_term += 1;
+    self_state.voted_for = Option::from(id);
 
-        let vote_request = VoteRequest {
-            term: self.current_term,
-            candidate_id: self.id,
+    let vote_request = VoteRequest {
+        term: self_state.current_term,
+        candidate_id: id,
+    };
+
+    let mut voted = 0;
+
+    let mut receivers = FuturesUnordered::new();
+    for node in &nodes {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        let cmd = Command::Vote {
+            request: vote_request.clone(),
+            resp: resp_tx,
         };
+        receivers.push(resp_rx);
+        node.send(cmd).await;
+    }
 
-        let mut voted = 0;
-
-        let mut receivers = FuturesUnordered::new();
-        for node in &self.nodes {
-            let (resp_tx, resp_rx) = oneshot::channel();
-            let cmd = Command::Vote {
-                request: vote_request.clone(),
-                resp: resp_tx,
-            };
-            receivers.push(resp_rx);
-            node.send(cmd);
-        }
-
-        while let Some(result) = receivers.next().await {
-            match result {
-                Ok(vote_response) => {
-                    if vote_response.term > self.current_term {
-                        self.state = State::Follower;
-                        self.current_term = vote_response.term;
-                        return;
-                    }
-
-                    if vote_response.vote_granted {
-                        voted += 1
-                    }
+    while let Some(result) = receivers.next().await {
+        match result {
+            Ok(vote_response) => {
+                if vote_response.term > self_state.current_term {
+                    self_state.election_state = ElectionState::Follower;
+                    self_state.current_term = vote_response.term;
+                    return;
                 }
-                Err(_) => {}
-            }
-        }
 
-        if voted >= self.nodes.len() / 2 {
-            self.state = State::Leader
+                if vote_response.vote_granted {
+                    voted += 1
+                }
+            }
+            Err(_) => {}
         }
     }
 
-    async fn process_vote_request(&mut self, vote_request: &VoteRequest) -> VoteResponse {
-        let mut vote_granted = false;
-        let mut term = 0;
+    if voted >= nodes.len() / 2 {
+        self_state.election_state = ElectionState::Leader
+    }
+}
 
-        if vote_request.term < self.current_term {
-            term = self.current_term;
-            vote_granted = false;
-        };
+async fn process_vote_request(
+    self_state: Arc<Mutex<State>>,
+    vote_request: &VoteRequest,
+) -> VoteResponse {
+    let mut vote_granted = false;
+    let mut term = 0;
+    let mut self_state = self_state.lock().await;
 
-        if vote_request.term == self.current_term {
-            match self.voted_for {
-                Some(id) => {
-                    if id == vote_request.candidate_id {
-                        vote_granted = true;
-                        term = vote_request.term;
-                    } else {
-                        vote_granted = false;
-                    }
-                }
-                // Should probably never happen, but just in case...
-                None => {
-                    self.voted_for = Option::from(vote_request.candidate_id);
-                    self.current_term = vote_request.term;
+    if vote_request.term < self_state.current_term {
+        term = self_state.current_term;
+        vote_granted = false;
+    };
+
+    if vote_request.term == self_state.current_term {
+        match self_state.voted_for {
+            Some(id) => {
+                if id == vote_request.candidate_id {
                     vote_granted = true;
                     term = vote_request.term;
+                } else {
+                    vote_granted = false;
                 }
             }
+            // Should probably never happen, but just in case...
+            None => {
+                self_state.voted_for = Option::from(vote_request.candidate_id);
+                self_state.current_term = vote_request.term;
+                vote_granted = true;
+                term = vote_request.term;
+            }
         }
-
-        if vote_request.term > self.current_term {
-            self.voted_for = Option::from(vote_request.candidate_id);
-            self.current_term = vote_request.term;
-            vote_granted = true;
-            term = vote_request.term;
-        }
-
-        VoteResponse { term, vote_granted }
     }
+
+    if vote_request.term > self_state.current_term {
+        self_state.voted_for = Option::from(vote_request.candidate_id);
+        self_state.current_term = vote_request.term;
+        vote_granted = true;
+        term = vote_request.term;
+    }
+
+    VoteResponse { term, vote_granted }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tokio::time::{sleep, Duration};
 
-    #[test]
-    fn server_new() {
+    #[tokio::test]
+    async fn server_new() {
         let server = Server::new(0);
         assert_eq!(0, server.id);
-        assert_eq!(State::Follower, server.state);
+        assert_eq!(
+            ElectionState::Follower,
+            server.state.lock().await.election_state
+        );
     }
 
     #[tokio::test]
     async fn request_vote_win_election() {
         let mut server = Server::new(0);
-        let mut node_1 = Server::new(1);
-        let mut node_2 = Server::new(2);
-        let mut node_3 = Server::new(3);
-        let mut node_4 = Server::new(4);
-        let nodes =
-            vec![node_1.tx.clone(), node_2.tx.clone(), node_3.tx.clone(), node_4.tx.clone()];
+        let node_1 = Server::new(1);
+        let node_2 = Server::new(2);
+        let node_3 = Server::new(3);
+        let node_4 = Server::new(4);
+        let nodes = vec![
+            node_1.tx.clone(),
+            node_2.tx.clone(),
+            node_3.tx.clone(),
+            node_4.tx.clone(),
+        ];
         server.add_nodes(nodes);
+        let state = server.state.clone();
 
-        select! {
-            () = server.start().fuse() => (),
-            () = server.request_vote().fuse() => (),
-        }
+        let nodes = server.nodes.clone();
+        let cloned_tx = server.tx.clone();
+        let shutdown = tokio::spawn(async move {
+            sleep(Duration::from_millis(100)).await;
+            for node in nodes {
+                node.send(Command::ShutDown {}).await;
+            }
+            cloned_tx.send(Command::ShutDown {}).await;
+        });
 
-        assert_eq!(State::Leader, server.state);
-        assert_eq!(1, server.current_term);
+        tokio::join!(
+            shutdown,
+            server.tx.send(Command::RequestVote {}),
+            start(
+                server.rx,
+                server.id,
+                server.nodes.clone(),
+                server.state.clone()
+            ),
+            start(
+                node_1.rx,
+                node_1.id,
+                node_1.nodes.clone(),
+                node_1.state.clone()
+            ),
+            start(
+                node_2.rx,
+                node_2.id,
+                node_2.nodes.clone(),
+                node_2.state.clone()
+            ),
+            start(
+                node_3.rx,
+                node_3.id,
+                node_3.nodes.clone(),
+                node_3.state.clone()
+            ),
+            start(
+                node_4.rx,
+                node_4.id,
+                node_4.nodes.clone(),
+                node_4.state.clone()
+            ),
+        );
+
+        let server_state = state.lock().await;
+        assert_eq!(ElectionState::Leader, server_state.election_state);
+        assert_eq!(1, server_state.current_term);
     }
-
-    // #[tokio::test]
-    // async fn request_vote_higher_term() {
-    //     let mut server = Server::new(0);
-    //     let mut node_mock = MockNode::new();
-    //     node_mock
-    //         .expect_process_vote_request()
-    //         .return_const(VoteResponse {
-    //             term: 42,
-    //             vote_granted: false,
-    //         });
-    //     let mut node_1 = Server::new(1);
-    //     let mut node_2 = Server::new(2);
-    //     let mut node_3 = Server::new(3);
-    //     let followers: Vec<&mut dyn Node> =
-    //         vec![&mut node_mock, &mut node_1, &mut node_2, &mut node_3];
-    //     server.add_followers(followers);
-    //
-    //     server.request_vote().await;
-    //
-    //     assert_eq!(State::Follower, server.state);
-    //     assert_eq!(42, server.current_term);
-    // }
 }
